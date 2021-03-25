d0e40c79885c871135e4ffca391ba0b5029040f1
Yan Garcia <yangarciavaz@gmail.com>
2021-03-25 00:23:08 -0300

    api created + cors

--- backend/app/Http/Controllers/ApiCrud/HomeController.php
+++ backend/app/Http/Controllers/ApiCrud/HomeController.php
@@ -7,7 +7,30 @@ use Illuminate\Http\Request;
 
 class HomeController extends Controller
 {
-  public function index(){
-      return response()->json(['status' => true]);
-  }
+    public function index()
+    {
+        $despesas = [
+            [
+                'id' => 1,
+                'nomeUsuario' => 'José Maria',
+                'descricaoDespesa' => 'Curso Vue',
+                'data' => '23-03-2021',
+                'imagemAnexo' => 'url...',
+                'custoDespesa' => '31',
+            ]
+        ];
+
+        $usuarios = [
+            [
+                'id' => 1,
+                'nomeUsuario' => 'José Maria',
+                'senhaUsuario' => '123456'
+            ]
+        ];
+
+        return response()->json([
+            'despesas' => $despesas,
+            'usuarios' => $usuarios,
+        ]);
+    }
 }
